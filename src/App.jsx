bed71a83cf1eeb3fc8592cb6dc17cecc7ff1647a
rogeriodos <rogeriodos@gmail.com>
2021-07-22 00:01:41 -0300

    ADD conceitos importantes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Counter } from './components/Counter';
+import { RepositoryList } from './components/RepositoryList';
+
 import './styles/global.scss';
 
 export function App() {
-    //throw new Error('Mensagem de erro padrão');
-
-    return <h1>Hello World!</h1>
+    return (
+        <>
+            <RepositoryList />
+            <Counter />
+        </>
+    )
 }
